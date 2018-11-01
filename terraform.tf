de4081c44c26ab4242ee5ae64294b8f608ddcd1b
liquidpredator <liquidpredator@gmail.com>
2018-11-02 02:29:31 +0300

    Some cleanup

--- terraform.tf
+++ terraform.tf
@@ -1,6 +1,5 @@
 variable "aws_access_key" {}
 variable "aws_secret_key" {}
-#variable "vpc_id" {}
 variable "ssh_secure_subnet" { type = "list" }
 variable "ssh_key" {}
 variable "ssh_pub_key" {}
@@ -11,17 +10,16 @@ variable "domain" {}
 provider "aws" {
   access_key = "${var.aws_access_key}"
   secret_key = "${var.aws_secret_key}"
-  region     = "us-west-2"
+  region     = "eu-central-1"
 }
 
 resource "aws_key_pair" "liquidpredator_hw5_key" {
   key_name = "liquidpredator_hw5_key"
-  public_key = "${var.ssh_pub_key}"
+  public_key = "${file(var.ssh_pub_key)}"
 }
 
 resource "aws_security_group" "liquidpredator_hw5_sg_front" {
   name = "liquidpredator_hw5_sg_front"
-#  vpc_id = "${var.vpc_id}"
 
   ingress {
     cidr_blocks = "${var.ssh_secure_subnet}"
@@ -59,39 +57,22 @@ resource "aws_security_group" "liquidpredator_hw5_sg_front" {
   }
 
   egress {
-    cidr_blocks = ["0.0.0.0/0"]
-    from_port = 32768
-    to_port = 65535
+    cidr_blocks = ["${aws_instance.backend.public_ip}/32"]
+    from_port = 8080
+    to_port = 8080
+    protocol = "tcp"
+  }
+
+  egress {
+    cidr_blocks = ["${aws_instance.backend.public_ip}/32"]
+    from_port = 9090
+    to_port = 9090
     protocol = "tcp"
   }
 }
 
-resource "aws_instance" "frontend" {
-  ami = "ami-0e32ec5bc225539f5"
-  instance_type = "t2.micro"
-  key_name = "${aws_key_pair.liquidpredator_hw5_key.key_name}"
-  associate_public_ip_address = true
-  security_groups = ["${aws_security_group.liquidpredator_hw5_sg_front.name}"]
-#  subnet_id = "${var.vpc_id}"
-
-  provisioner "remote-exec" {
-    inline = ["sudo cp /home/ubuntu/.ssh/authorized_keys /root/.ssh/"]
-    connection {
-      type     = "ssh"
-      user     = "ubuntu"
-#      private_key = "file(${var.ssh_key})"
-    }
-  }
-
-  provisioner "remote-exec" {
-    inline = ["apt update && apt install -y python2.7-minimal && update-alternatives --install /usr/bin/python python /usr/bin/python2.7 1"]
-  }
-
-}
-
 resource "aws_security_group" "liquidpredator_hw5_sg_back" {
   name = "liquidpredator_hw5_sg_back"
-#  vpc_id = "${var.vpc_id}"
 
   ingress {
     cidr_blocks = "${var.ssh_secure_subnet}"
@@ -129,27 +110,55 @@ resource "aws_security_group" "liquidpredator_hw5_sg_back" {
   }
 
   egress {
-    cidr_blocks = ["0.0.0.0/0"]
-    from_port = 32768
-    to_port = 65535
+    cidr_blocks = ["${aws_instance.frontend.public_ip}/32"]
+    from_port = 8080
+    to_port = 8080
+    protocol = "tcp"
+  }
+
+  egress {
+    cidr_blocks = ["${aws_instance.frontend.public_ip}/32"]
+    from_port = 9090
+    to_port = 9090
     protocol = "tcp"
   }
 }
 
-resource "aws_instance" "backend" { 
-  ami = "ami-0e32ec5bc225539f5"
+resource "aws_instance" "frontend" {
+  ami = "ami-086a09d5b9fa35dc7"
   instance_type = "t2.micro"
   key_name = "${aws_key_pair.liquidpredator_hw5_key.key_name}"
   associate_public_ip_address = true
-  security_groups = ["${aws_security_group.liquidpredator_hw5_sg_back.name}"]
-#  subnet_id = "${var.vpc_id}"
+  security_groups = ["${aws_security_group.liquidpredator_hw5_sg_front.name}"]
 
   provisioner "remote-exec" {
     inline = ["sudo cp /home/ubuntu/.ssh/authorized_keys /root/.ssh/"]
     connection {
       type     = "ssh"
       user     = "ubuntu"
-#      private_key = "file(${var.ssh_key})"
+      private_key = "${file(var.ssh_key)}"
+    }
+  }
+
+  provisioner "remote-exec" {
+    inline = ["apt update && apt install -y python2.7-minimal && update-alternatives --install /usr/bin/python python /usr/bin/python2.7 1"]
+  }
+
+}
+
+resource "aws_instance" "backend" { 
+  ami = "ami-086a09d5b9fa35dc7"
+  instance_type = "t2.micro"
+  key_name = "${aws_key_pair.liquidpredator_hw5_key.key_name}"
+  associate_public_ip_address = true
+  security_groups = ["${aws_security_group.liquidpredator_hw5_sg_back.name}"]
+
+  provisioner "remote-exec" {
+    inline = ["sudo cp /home/ubuntu/.ssh/authorized_keys /root/.ssh/"]
+    connection {
+      type     = "ssh"
+      user     = "ubuntu"
+      private_key = "${file(var.ssh_key)}"
     }
   }
 
@@ -166,7 +175,7 @@ resource "aws_route53_record" "liquidpredator_hw5_dns" {
   records = ["${aws_instance.frontend.public_ip}"]
 }
 
-resource "null_resource" "liquidpredator_hw5" {
+resource "null_resource" "liquidpredator_hw5_deploy" {
   provisioner "local-exec" {
     command = "touch frontvars.yml && :> frontvars.yml && echo 'set_backend_ip: \"${aws_instance.backend.public_ip}\"' >> frontvars.yml && echo 'set_domain_name: \"${var.domain}\"' >> frontvars.yml && echo 'certbot_admin_email: \"${var.admin_mail}\"' >> frontvars.yml"
   }
